0a841e2bed39aa67ab363f24ea91320ed45d2f1e
Mitchell45432 <111002111+Mitchell45432@users.noreply.github.com>
2022-08-13 08:52:00 -0400

    i

--- pages/components/nftCard.jsx
+++ pages/components/nftCard.jsx
@@ -20,12 +20,10 @@ export const NFTCard = ({ nft }) => {
         //description appears in hover-over the NFTs picture to save screen footprint 
         <div className="w-1/6 m-3 flex flex-col ">
         <div className="rounded-md">
-            <image 
+            <image className="objectFit-cover h-128 w-full rounded-t-md" 
             src={nft.media[0].gateway} 
             alt={nft.description} title={nft.description}
-            height={150}
-            widtht={150}
-            />
+            ></image>
         </div>
         <div className="flex flex-col y-gap-2 px-2 py-3 bg-purple-600 rounded-b-md h-110 ">
             <div className="">
